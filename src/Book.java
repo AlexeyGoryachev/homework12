6f739d235ed8ed59d492f1450d70fc2213061fc7
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-03-24 23:19:04 +0300

    wrote the code for the "Book" class.

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,27 @@
 public class Book {
+    private String bookTitle;
+    private Author authorsName;
+    private int yearOfPublishing;
+
+    public Book(String bookTitle, Author authorsName, int yearOfPublishing) {
+        this.authorsName = authorsName;
+        this.bookTitle = bookTitle;
+        this.yearOfPublishing = yearOfPublishing;
+    }
+
+    public Author getAuthorsName() {
+        return this.authorsName;
+    }
+
+    public String getBookTitle() {
+        return this.bookTitle;
+    }
+
+    public int getYearOfPublishing() {
+        return this.yearOfPublishing;
+    }
+
+    public void setYearOfPublishing(int yearOfPublishing) {
+        this.yearOfPublishing = yearOfPublishing;
+    }
 }
