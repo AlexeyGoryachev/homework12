0304b6dfa007af29ab33d7bece5bcca915b731db
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-03-28 20:37:36 +0300

    corrected

--- src/Book.java
+++ src/Book.java
@@ -26,5 +26,16 @@ public class Book {
     public void setYearOfPublishing(int yearOfPublishing) {
         this.yearOfPublishing = yearOfPublishing;
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if(o == null ||getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(authorsName, book.authorsName) && Objects.equals(bookTitle, book.bookTitle) && Objects.equals(yearOfPublishing, book.yearOfPublishing);
+    }
 
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(authorsName, bookTitle, yearOfPublishing);
+    }
 }
