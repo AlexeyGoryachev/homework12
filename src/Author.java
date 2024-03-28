dfef55296249b7ed82b12832c0aaa663e9c03af7
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-03-28 12:36:06 +0300

    corrected

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String surName;
@@ -14,5 +16,18 @@ public class Author {
     }
     public String toString() {
         return this.name + " " + this.surName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surName, author.surName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surName);
     }
 }
